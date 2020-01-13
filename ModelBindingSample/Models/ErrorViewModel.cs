3de90a779f9dbc6ec3d761469a143c3105f7bbb0
davidrodjen <davidrodriguezjenkins@gmail.com>
2020-01-13 12:39:20 -0800

    Add project files.

---
+++ ModelBindingSample/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ModelBindingSample.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ ModelBindingSample/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ModelBindingSample.Models
+{
+    public class Student
+    {
+        //Prop is keyword to auto
+        public int StudentId { get; set; }
+
+        public string FullName { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public int FavoriteNumber { get; set; }
+
+    }
+}
