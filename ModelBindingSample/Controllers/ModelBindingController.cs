ecd680b82904fb79a6701bdf34c97dfe4661b79d
davidrodjen <davidrodriguezjenkins@gmail.com>
2020-01-14 11:40:22 -0800

    Created student modelbinding

---
+++ ModelBindingSample/Controllers/ModelBindingController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ModelBindingSample.Models;
+
+namespace ModelBindingSample.Controllers
+{
+    public class ModelBindingController : Controller
+    {
+        [HttpGet] //Default, not required
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Student s) //Have to set up a using to use Student using ModelBindingSample.Model
+        {
+            // Ensures model is in valid state
+            if (ModelState.IsValid)
+            {
+                //StudentDB.add(s); (((example for the assignment)))
+                return RedirectToAction("Index", "Home");
+            }
+            else // Validation problems
+            {
+                // You CAN pass the model back to the view
+                // but this is now done automatically
+                // by just returning the view
+                return View(s);
+            }
+        }
+    }
+}
--- ModelBindingSample/Models/Student.cs
+++ ModelBindingSample/Models/Student.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,11 +11,18 @@ namespace ModelBindingSample.Models
         //Prop is keyword to auto
         public int StudentId { get; set; }
 
+        [Display(Name = "Full Name")] //have to add using System.ComponentModel.DataAnnotations
+        [Required(ErrorMessage = "Full Name is required")] //If not passing anything, don't have to have ()
         public string FullName { get; set; }
 
+        [Display(Name = "DOB")]
+        [DataType(DataType.Date)]
         public DateTime DateOfBirth { get; set; }
 
-        public int FavoriteNumber { get; set; }
+        [Display(Name = "Favorite Number")] //These are all Data Annotations
+        [Range(1, 100, ErrorMessage = "Choose between 1 and 100")]
+        [Required] // int is always required because it is a value type
+        public int FavoriteNumber { get; set; } //ints are required by default
 
     }
 }
